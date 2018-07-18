36a88aca9ff052e655e2c8b59f3a1bf80d3e81c2
yeojoy <yeojoy.kim@gmail.com>
2018-07-18 11:50:32 +0900

    add POST verb with response status

--- app/app.py
+++ app/app.py
@@ -5,15 +5,23 @@ app = Flask(__name__)
 api = Api(app)
 
 items = [
-    {'name': 'yeojoy'}
+    {'name': 'good stub', 'price': 13.99}
 ]
 
 class Item(Resource):
+
     def get(self, name):
         for item in items:
-            return jsonify({'item': item})
+            if item['name'] == name:
+                print("name :" + name)
+                return {'item': item}, 200
 
-        return jsonify({'message': 'item not found.'})
+        return {'message': 'item not found.'}, 404
+
+    def post(self, name):
+        new_item = {'name': name, 'price': 12.99}
+        items.append(new_item)
+        return new_item, 201
 
 api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1/5000/item/yeojoy
 
