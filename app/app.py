c6ed806262d7e83f5e0ca4eb467f657f466a69dd
yeojoy <yeojoy.kim@gmail.com>
2018-07-18 11:21:49 +0900

    add memory db
    and change model

--- app/app.py
+++ app/app.py
@@ -4,10 +4,17 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return jsonify({'student': name})
+items = [
+    {'name': 'yeojoy'}
+]
 
-api.add_resource(Student, '/student/<string:name>') # http://127.0.0.1/5000/student/yeojoy
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            return jsonify({'item': item})
+
+        return jsonify({'message': 'item not found.'})
+
+api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1/5000/item/yeojoy
 
 app.run(host='0.0.0.0', port=5000)
