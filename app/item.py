94de2e9f57d0e766060e34ba52b9e3d8d860b082
yeojoy <yeojoy.kim@gmail.com>
2018-07-23 10:10:55 +0900

    update all verbs to use DB

--- app/item.py
+++ app/item.py
@@ -51,16 +51,32 @@ class Item(Resource):
         data = Item.parser.parse_args()
         new_item = {'name': name, 'price': data['price']}
         # items.append(new_item)
+        try:
+            self.insert(new_item)
+        except:
+            return {"message": "An error occurred inserting the item."}, 500 # Internal server error
+        
+        return new_item, 201
 
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect("my_app.db")
         cursor = connection.cursor()
 
         query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (new_item['name'], new_item['price'], ))
+        cursor.execute(query, (item['name'], item['price'], ))
         connection.commit()
         connection.close()
 
-        return new_item, 201
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect("my_app.db")
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price = ? WHERE name = ?"
+        cursor.execute(query, (item['price'], item['name'],))
+        connection.commit()
+        connection.close()
 
     @jwt_required()
     def put(self, name):
@@ -77,14 +93,24 @@ class Item(Resource):
         #return {'message': 'item not found'}, 404
 
         data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
+        # item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
 
-        return item
+        if item is None:
+            # item = {'name': name, 'price': data['price']}
+            # items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occurred inserting the itme."}, 500
+        else:
+            try:
+                self.update(updated_item)
+            except:
+                return {"message": "An error occurred updating the itme."}, 500
+
+        return updated_item
 
 
     @jwt_required()
@@ -96,22 +122,33 @@ class Item(Resource):
         # 
         # return {'message': 'item not found'}, 404
         
-        global items # python thinks items is local variable. so add "global" keyword
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'success to delete item.'}, 200
+        # global items # python thinks items is local variable. so add "global" keyword
+        # items = list(filter(lambda x: x['name'] != name, items))
+
+        connection = sqlite3.connect("my_app.db")
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name = ?"
+        cursor.execute(query, (name, ))
+        
+        connection.commit()
+        connection.close()
+
+        return {'message': 'Item deleted.'}
 
 
 class ItemList(Resource):
+    @jwt_required()
     def get(self):
         connection = sqlite3.connect('my_app.db')
         cursor = connection.cursor()
 
         query = "SELECT * from items"
         result = cursor.execute(query)
-        rows = result.fetchall()
         items = []
-        for row in rows:
-            item = {'name': row[0], 'price': row[1]}
-            items.append(item)
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
 
+        connection.close()
+        
         return {'items': items}, 200
