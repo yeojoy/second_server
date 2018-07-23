7191cfc7a13b295d3fd473efa090ac3ec2413552
yeojoy <yeojoy.kim@gmail.com>
2018-07-23 10:57:33 +0900

    refactorying package

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,4 @@
 import sqlite3
-from flask_restful import Resource, reqparse
 
 class User:
     def __init__(self, _id, username, password):
@@ -40,36 +39,3 @@ class User:
         
         connection.close()
         return user
-
-class UserRegister(Resource):
-
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-        type = str,
-        required = True,
-        help = 'This field cannot be blank.'
-    )
-
-    parser.add_argument('password',
-        type = str,
-        required = True,
-        help = 'This field cannot be blank.'
-    )
-
-    def post(self):
-        
-        data = UserRegister.parser.parse_args()
-
-        if User.find_by_username(data['username']):
-            return {'message': 'A user with that username already exists.'}, 400
-
-        connection = sqlite3.connect('my_app.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)" # "id" is auto increment, so set NULL.
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
-
-        return {'message': 'User created successfully.'}, 201
---
+++ app/resources/user.py
@@ -0,0 +1,36 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import User
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type = str,
+        required = True,
+        help = 'This field cannot be blank.'
+    )
+
+    parser.add_argument('password',
+        type = str,
+        required = True,
+        help = 'This field cannot be blank.'
+    )
+
+    def post(self):
+        
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):
+            return {'message': 'A user with that username already exists.'}, 400
+
+        connection = sqlite3.connect('my_app.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)" # "id" is auto increment, so set NULL.
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'User created successfully.'}, 201
